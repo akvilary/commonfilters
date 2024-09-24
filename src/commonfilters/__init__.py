22e9eac1ede1de8eb862892d69c92143d3c05005
akvilary <abc-sm@yandex.ru>
2024-09-24 22:40:49 +0800

    0.0.3: Add new features

--- src/commonfilters/__init__.py
+++ src/commonfilters/__init__.py
@@ -1,1 +1,9 @@
-from .filters import Filter, Filters
+from .filter_coupling_policy import FilterCouplingPolicy
+from .filters import (
+    apply_filters,
+    apply_filters_with_and_policy,
+    apply_filters_with_or_policy,
+    Filter,
+    CompoundFilter,
+    Filters,
+)
